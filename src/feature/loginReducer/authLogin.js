46a3438c6f6873ac2828f18412b0b82a1dee2445
Jonah <jonah@essopl.in>
2023-03-02 13:07:33 +0530

    login url change

--- src/feature/loginReducer/authLogin.js
+++ src/feature/loginReducer/authLogin.js
@@ -1,10 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../../constants/ConstaltsVariables";
 
 // Define a service using a base URL and expected endpoints
 export const ecommerceLoginApi = createApi({
   reducerPath: "couponEnginesLoginApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5001/",
+    baseUrl: BASE_URL,
   }),
   tagTypes: ["Auth", "GetMenus"],
   endpoints: (builder) => ({
